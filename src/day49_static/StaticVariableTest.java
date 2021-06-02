a8d2f0da17615ecf491675853a2951f376725977
eyubee <teferaey@gmail.com>
2021-06-01 21:17:05 -0400

    new class

---
+++ src/day49_static/StaticVariableTest.java
@@ -0,0 +1,9 @@
+package day49_static;
+
+public class StaticVariableTest {
+    public static void main(String[] args) {
+        System.out.println(StaticVariables.name);
+        System.out.println(StaticVariables.userName);
+        System.out.println(StaticVariables.age);
+    }
+}
