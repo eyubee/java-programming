67ed81bde17b7dd232b4c9e1ee31fc0d511740de
eyubee <teferaey@gmail.com>
2021-06-05 11:13:16 -0400

    inital commit

---
+++ src/day50_inheritance/StaticBlockDemo.java
@@ -0,0 +1,10 @@
+package day50_inheritance;
+
+public class StaticBlockDemo {
+    static {
+        System.out.println("static initializer block");
+    }
+    public StaticBlockDemo(){
+        System.out.println("constructor method");
+    }
+}
