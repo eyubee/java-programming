42fb41c7dfffa923a1f1126676d57d28ea6e6c68
eyubee <teferaey@gmail.com>
2021-06-05 11:48:32 -0400

    added initializer block and num

--- src/day50_inheritance/StaticBlockDemo.java
+++ src/day50_inheritance/StaticBlockDemo.java
@@ -1,10 +1,26 @@
 package day50_inheritance;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class StaticBlockDemo {
+    static int num;
+    static List<String> carModels;
     static {
         System.out.println("static initializer block");
+        num = 10;
+        carModels= new ArrayList<>();
+        carModels.addAll(Arrays.asList("Toyota", "Honda","lexus"));
+
+    }
+    {
+//initializer block...runs each time objevt is created, before construcator
+        System.out.println("initializer block");
     }
     public StaticBlockDemo(){
         System.out.println("constructor method");
+        num +=5;
+
     }
 }
--- src/day50_inheritance/StaticBlockTest.java
+++ src/day50_inheritance/StaticBlockTest.java
@@ -2,9 +2,10 @@ package day50_inheritance;
 
 public class StaticBlockTest {
     public static void main(String[] args) {
-        StaticBlockDemo st1= new StaticBlockDemo();//static initializer and constructor
-        StaticBlockDemo st2= new StaticBlockDemo();//constructor
-        StaticBlockDemo st3= new StaticBlockDemo();//constructor
+        StaticBlockDemo st1= new StaticBlockDemo();//static initializer and constructor(10+5)
+        StaticBlockDemo st2= new StaticBlockDemo();//constructor (15+5)
+        StaticBlockDemo st3= new StaticBlockDemo();//constructor (20+5) each constructor adds 5
+        System.out.println(StaticBlockDemo.num);
 
     }
 }
