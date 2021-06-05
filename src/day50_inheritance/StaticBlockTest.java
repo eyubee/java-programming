6dd9e0b6e66aed48dd0b80b1a57f0f5a1e5d9176
eyubee <teferaey@gmail.com>
2021-06-05 11:23:42 -0400

    added constructor

--- src/day50_inheritance/StaticBlockTest.java
+++ src/day50_inheritance/StaticBlockTest.java
@@ -2,8 +2,9 @@ package day50_inheritance;
 
 public class StaticBlockTest {
     public static void main(String[] args) {
-        StaticBlockDemo st1= new StaticBlockDemo();
-        StaticBlockDemo st2= new StaticBlockDemo();
+        StaticBlockDemo st1= new StaticBlockDemo();//static initializer and constructor
+        StaticBlockDemo st2= new StaticBlockDemo();//constructor
+        StaticBlockDemo st3= new StaticBlockDemo();//constructor
 
     }
 }
